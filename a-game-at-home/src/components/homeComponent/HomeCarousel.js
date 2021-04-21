28a8c731c6f92fbeb6c64acbc4bf61c304f0a80d
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-04-21 11:59:09 +0200

    Carousel OK

--- a-game-at-home/src/components/homeComponent/HomeCarousel.js
+++ a-game-at-home/src/components/homeComponent/HomeCarousel.js
@@ -1,13 +1,27 @@
 import React from "react";
+import Carousel from "react-elastic-carousel";
+import "../../assets/css/carousel.min.css";
 
 class HomeCarousel extends React.Component {
+  state = {
+    items: [
+      {id: 1, title: 'item #1', image: process.env.PUBLIC_URL + "/img/carousel1.jpg"},
+      {id: 2, title: 'item #2', image: process.env.PUBLIC_URL + "/img/carousel2.jpg"},
+      {id: 3, title: 'item #3', image: process.env.PUBLIC_URL + "/img/carousel3.jpg"}
+    ]
+  }
 
-  render() {
+  render () {
+    const { items } = this.state;
     return (
-      <div>
-        Home carousel
-      </div>
-    );
+      <Carousel enableAutoPlay={true} autoPlaySpeed={10000}>
+        {items.map(item => (
+          <div key={item.id} className="carouselContainer" style={{backgroundImage: `url(${item.image})`}}>
+            <p className="is-size-2-tablet">{item.title}</p>
+          </div>
+        ))}
+      </Carousel>
+    )
   }
 }
 
