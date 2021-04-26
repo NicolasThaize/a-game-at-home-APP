b8b533a030145654ab8301a4bb249e91fff9942b
Nicolas THAIZE <nicolas.thaize@ynov.com>
2021-04-26 16:52:28 +0200

    git push all register fields

--- a-game-at-home/src/components/authComponents/registerComponents/Register.js
+++ a-game-at-home/src/components/authComponents/registerComponents/Register.js
@@ -7,13 +7,18 @@ class Register extends React.Component{
   render() {
     return(
       <div>
-        <Input inputValues={ {label: 'tst', placeholder: 'test', type:"password"} }/>
+        <Input inputValues={ {label: 'Nom', placeholder: 'Marlo', type:"text"} }/>
+        <Input inputValues={ {label: 'Prenom', placeholder: 'Grégoire', type:"text"} }/>
+        <Input inputValues={ {label: 'Nom d\'utilisateur', placeholder: 'Grégoire12', type:"text"} }/>
+        <Input inputValues={ {label: 'Adresse Email', placeholder: 'exemple@gmail.com', type:"email"} }/>
+        <Input inputValues={ {label: 'Mot de passe', placeholder: '**********', type:"password"} }/>
+        <Input inputValues={ {label: 'Confirmation du mot de passe', placeholder: '**********', type:"password"} }/>
 
         <div className="field">
           <div className="control">
             <label className="checkbox">
               <input type="checkbox"/>
-                J'accepte les <Link to="/">termes d'utilisation</Link>.
+              &nbsp;J'accepte les <Link to="/">conditions d'utilisation</Link>.
             </label>
           </div>
         </div>
