103b2535fd0fa8869a8b1b86deb02d97e0d28e0c
Oui <leo.masseglia@ynov.com>
2021-04-26 17:04:16 +0200

    Le gros zozo

---
+++ a-game-at-home/src/components/authComponents/loginComponents/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Input from "../Input"
+
+class Login extends React.Component{
+    render(){
+        return(
+
+
+
+
+
+
+        )
+    }
+}
+
+export default Login;
