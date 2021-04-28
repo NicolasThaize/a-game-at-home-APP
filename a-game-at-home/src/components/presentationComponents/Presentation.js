98b85fc69eb4a27b1e25f4299095b44400544ef7
Oui <leo.masseglia@ynov.com>
2021-04-28 18:54:13 +0200

    Presentation done

--- a-game-at-home/src/components/presentationComponents/Presentation.js
+++ a-game-at-home/src/components/presentationComponents/Presentation.js
@@ -9,10 +9,10 @@ class Presentation extends React.Component{
                 <hr/>
                 <blockquote>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquam, lacus quis commodo ornare, enim
-                    metus sollicitudin enim, ac aliquet mauris tellus quis mi.<i>Karim Binon</i>
+                    metus sollicitudin enim, ac aliquet mauris tellus quis mi.<i>- Karim Binon</i>
                 </blockquote>
                 <div className="tile is-ancestor">
-                    <div className="tile is-vertical radius-rounded is-8">
+                    <div className="tile is-vertical is-8">
                         <div className="tile">
                             <div className="tile is-parent is-vertical">
                                 <article className="tile is-child notification is-primary">
@@ -25,13 +25,9 @@ class Presentation extends React.Component{
                                 </article>
                             </div>
                             <div className="tile is-parent">
-                                <article className="tile is-child notification is-info">
-                                    <p className="title">Middle tile</p>
-                                    <p className="subtitle">With an image</p>
-                                    <figure className="image is-4by3">
-                                        <img src="https://bulma.io/images/placeholders/640x480.png" alt="sample"/>
-                                    </figure>
-                                </article>
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/640x480.png" alt="sample"/>
+                                </figure>
                             </div>
                         </div>
                         <div className="tile is-parent">
@@ -44,13 +40,12 @@ class Presentation extends React.Component{
                             </article>
                         </div>
                     </div>
-                    <div className="tile is-parent">
-                        <article className="tile is-child notification is-success">
+                    <div className="tile is-parent" style={{backgroundImage:"url('/img/mountain-png.png')",backgroundPosition:"center",backgroundSize:"cover"}}>
+                        <article className="tile is-child">
                             <div className="content">
                                 <p className="title">Tall tile</p>
                                 <p className="subtitle">With even more content</p>
                                 <div className="content">
-
                                 </div>
                             </div>
                         </article>
